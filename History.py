85d9b8c4310f18a40d32a51aa6518fe95ed62102
Binaya764 <bigyanlimbu179@gmail.com>
2025-11-03 07:14:54 +0545

    history added

---
+++ History.py
@@ -0,0 +1,9 @@
+class Back_history:
+    def show_history():
+        print("History function called.")
+
+
+        """with open ("History.txt", 'a') as f:
+            pass"""
+    def clear():
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #includes the calcuatio part of the calculator
+from History import Back_history
 class User:
     def calculations(self,num1,num2):
             option = input("Enter the operation you want to perform (add,subtract,multiply,divide):")
@@ -36,8 +37,9 @@ class User:
     
     def User_value(self,data):
        
-            if(data == "history"):
-                   pass
+            if(data.lower() == "history"):
+                   Back_history.show_history()
+
             elif(data == "clear"):
                    pass
             else:
